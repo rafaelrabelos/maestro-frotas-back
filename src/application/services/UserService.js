4d5686b01f5ee36ff1d9eab1431fcc0a8aa00bab
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-04-06 22:08:12 -0300

    Obtem e valida usuários no service

--- src/application/services/UserService.js
+++ src/application/services/UserService.js
@@ -4,9 +4,37 @@ const UsuarioRepository = require("../../infra/database/repository/UserRepositor
 async function GetUsers(){
 
   var users = await UsuarioRepository.GetAll();
-
-  return users;
-
+  return ValidateManyUser(users);
 }
 
-module.exports = {GetUsers}
+async function GetUserById(id){
+
+  var user = await UsuarioRepository.GetById(id);
+
+  return(ValidateSingleUser(user));
+}
+
+async function GetUserWithRolesById(id){
+
+  var user = await UsuarioRepository.GetWithRolesById(id);
+  return(ValidateSingleUser(user));
+}
+
+
+async function ValidateSingleUser(user){
+  if(Array.isArray(user) && user.length == 1){
+    return user[0];
+  }
+
+  return null;
+}
+
+async function ValidateManyUser(users){
+  if(Array.isArray(users) && users.length >= 0){
+    return users;
+  }
+
+  return null;
+}
+
+module.exports = {GetUsers, GetUserById, GetUserWithRolesById}
