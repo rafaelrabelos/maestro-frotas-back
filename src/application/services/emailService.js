a673d2b2b7737df5aaa73d2fd882917c17ce4b86
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-04-09 20:55:03 -0300

    implementa o recovery de senha no serviço de email

--- src/application/services/emailService.js
+++ src/application/services/emailService.js
@@ -1,10 +1,44 @@
-const EmailAdapter = require('../../infra/adapters/emailAdapter');
+const Mail = require('../../infra/adapters/emailAdapter');
+const util = require('../../util/libs/datetime');
 
 async function SendRecoverEmail(user, code) {
 
-  console.log(`======> ${code}`);
-  // TODO implementar o adpter de email
+  const from = {
+    name: 'Frotas',
+    email: 'frotas@intellicondo.xyz'
+  }
 
-  return EmailAdapter.Send(user.email, code);
+  const to = {
+    name: user.nome,
+    email: user.email.replace(/[^a-zA-Z0-9@-_.]/gi, ''),//user.email
+  }
+  
+  const message = {
+    from: from,
+    to: to ,
+    subject: 'Recuperação de senha',
+    message: `
+    Foi solicitada a redefinição de senha para seu CPF/E-mail.
+    Segue o seu código de recuperação. utilize-o para redefinir sua senha na pagina de recuperação`,
+    body: "",
+  }
+
+  message.body = `Olá, <b>${message.to.name}</b>!${message.to.name}<br/><br/>
+  <p>${message.message}</P>
+  <br/><br/>
+  <p><b>${code}</b></P>
+  <br/>
+  <p>
+  Criado em: ${util.dataTempoFormatada(user.created_at)}<br/>
+  Valido até: ${util.dataTempoFormatada(user.valid_date)}<br/>
+  </p>
+  <p> Atenciosamente,<br/>
+  Maestro Frotas.
+  `;
+
+  var resultAdapter = await Mail.defaultAdapter.Send(message);
+
+  return resultAdapter;
 }
+
 module.exports = { SendRecoverEmail };
