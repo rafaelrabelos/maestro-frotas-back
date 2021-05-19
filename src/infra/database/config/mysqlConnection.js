e8dba7d34ff5ecedb02334bea3221ffeb45d1794
Rafael A. Rabelo <rafael.rabelo@thoughtworks.com>
2021-05-18 22:41:24 -0300

    Adiciona validação na conexão do mysql

--- src/infra/database/config/mysqlConnection.js
+++ src/infra/database/config/mysqlConnection.js
@@ -4,15 +4,21 @@ async function mysqlConnection(){
   if(global.mysqlConnection && global.mysqlConnection.state !== 'disconnected')
       return global.mysqlConnection;
 
-  const connection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS,
-    database: process.env.MYSQL_DATABASE,
-  });
-  
-  global.mysqlConnection = connection;
-  return connection;
+      try {
+        const connection = await mysql.createConnection({
+          host: process.env.MYSQL_HOST,
+          user: process.env.MYSQL_USER,
+          password: process.env.MYSQL_PASS,
+          database: process.env.MYSQL_DATABASE,
+        });
+        global.mysqlConnection = connection;
+        return connection;
+        
+      } catch (error) {
+        console.log(error.message);
+        global.mysqlConnection = null;
+        return null;
+      }
 }
 
 module.exports = mysqlConnection;
